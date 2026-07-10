a8e70144e2cc71c791aa61ad042c0d1891a3bce9
milanofthe <milanofthe@users.noreply.github.com>
2026-07-10 21:17:03 +0200

    release: v0.3.0 - Versionen vereinheitlicht (workspace, python-Paket, __version__)

--- python/python_src/rapidmesh/__init__.py
+++ python/python_src/rapidmesh/__init__.py
@@ -15,7 +15,7 @@ from . import adapt
 from .adapt import dorfler_mark, refine_dorfler
 from ._native import set_log_level
 
-__version__ = "0.1.0"
+__version__ = "0.3.0"
 __all__ = [
     "Geometry",
     "Mesh",
